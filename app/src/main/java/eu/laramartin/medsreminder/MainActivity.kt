fab2f29b2032d949d6a9ea03a38eea54a4d5acc3
Lara Martin <larikimc@gmail.com>
2017-07-08 10:55:16 +0200

    convert MainActivity from java to kt

---
+++ app/src/main/java/eu/laramartin/medsreminder/MainActivity.kt
@@ -0,0 +1,12 @@
+package eu.laramartin.medsreminder
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
