ab0e885a698a6d9c22578f965611db87aefce4c3
Lara Martin <larikimc@gmail.com>
2017-07-08 11:33:04 +0200

    setup authentification

--- app/src/main/java/eu/laramartin/medsreminder/MainActivity.kt
+++ app/src/main/java/eu/laramartin/medsreminder/MainActivity.kt
@@ -1,12 +1,69 @@
 package eu.laramartin.medsreminder
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.firebase.ui.auth.ResultCodes
+import com.google.firebase.auth.FirebaseAuth
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            // already signed in
+        } else {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(
+                                    arrayListOf(AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == ResultCodes.OK) {
+                // TODO login complete
+                Toast.makeText(this, "login successful", Toast.LENGTH_SHORT).show()
+                return
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "back button", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                if (response.errorCode == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "no network", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                if (response.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, "unknown error", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+            Toast.makeText(this, "unknown sign in", Toast.LENGTH_SHORT).show()
+        }
     }
 }
