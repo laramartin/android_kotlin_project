5d8127d0cf7913ea0dff0c86638fb2127aba746e
Lara Martin <larikimc@gmail.com>
2017-07-08 12:36:45 +0200

    read meds in DB

--- app/src/main/java/eu/laramartin/medsreminder/DatabaseActivity.kt
+++ app/src/main/java/eu/laramartin/medsreminder/DatabaseActivity.kt
@@ -2,10 +2,16 @@ package eu.laramartin.medsreminder
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DatabaseActivity : AppCompatActivity() {
+
+    val LOG_TAG: String = DatabaseActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +23,27 @@ class DatabaseActivity : AppCompatActivity() {
                     .child("medTest")
                     .setValue(Med("medTest", "Mo-We", "22:00"))
 
+            database.child(user.uid)
+                    .child("meds")
+                    .addListenerForSingleValueEvent(object: ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError?) {
+                            Log.e(LOG_TAG, "error ${p0?.message}")
+                        }
+
+                        override fun onDataChange(p0: DataSnapshot?) {
+                            p0?.children?.forEach {
+                                val med = it.getValue(Med::class.java)
+                                Log.i(LOG_TAG, med.toString())
+                            }
+                        }
+                    })
 
         }
     }
 }
 
 data class Med(
-        val name: String,
-        val daysOfWeek: String,
-        val time: String
+        var name: String = "",
+        var daysOfWeek: String = "",
+        var time: String = ""
 )
